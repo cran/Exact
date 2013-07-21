610c9e37c48f21d30473116108db65dcb1d4442e
Peter Calhoun <calhoun.peter@gmail.com>
2013-07-21 00:00:00 +0000

    version 1.4

--- R/binomialCode.R
+++ R/binomialCode.R
@@ -1,5 +1,37 @@
 binomialCode <-
-function(data,alternative="two.sided",npNumbers=100,beta=0.001,interval=FALSE,method="Z-pooled",cond.row=TRUE,to.plot=TRUE,ref.pvalue=TRUE){
+function(data,alternative="two.sided",npNumbers=100,beta=.001,interval=FALSE,method="Z-pooled",cond.row=TRUE,to.plot=TRUE,ref.pvalue=TRUE){
+
+maxPvalue<-function(tbls){
+   #Instead of calculating the binomial probabilities several times for a more extreme cell,
+   #calculate the probability once and then combine with other more extreme cells:
+   xTbls<-tbls[,1]
+   yTbls<-tbls[,2]
+   nTbls<-length(xTbls) # Number of 'as or more extreme' tables
+
+   x.unique<-unique(xTbls)
+   y.unique<-unique(yTbls)
+   lxu<-length(x.unique)
+   lyu<-length(y.unique)
+
+   xnr<-max(xTbls)+1
+   A<-matrix(nrow=xnr, ncol=npNumbers)
+   cellnr<-rep.int(x.unique+1, npNumbers)+xnr*rep(seq(npNumbers)-1, each=lxu)
+   A[cellnr]<-dbinom(rep.int(x.unique, npNumbers), Ns[1], rep(int, each=lxu))
+
+   ynr<-max(yTbls)+1
+   B<-matrix(nrow=ynr, ncol=npNumbers)
+   cellnr<-rep.int(y.unique+1, npNumbers)+ynr*rep(seq(npNumbers)-1, each=lyu)
+   B[cellnr]<-dbinom(rep.int(y.unique, npNumbers), Ns[2], rep(int, each=lyu))
+
+   prob<-matrix(A[xTbls+1,]*B[yTbls+1,],ncol=length(int))
+   prob<-colSums(prob, nTbls)
+   
+   np<-int[which(prob==max(prob))]
+   pvalue<-max(prob)+beta
+
+   list<-list(prob=prob,pvalue=pvalue,np=np)
+   return(list)
+}
 
 #If conditioning on row, then transpose 2x2 table
 if(cond.row){data<-t(data)}
@@ -23,7 +55,6 @@ if(interval){
       length=npNumbers)
    }
 } else {int<-seq(0.00001,.99999,length=npNumbers); beta<-0}
-
 
 x <- rep(0:Ns[1], each=(Ns[2]+1))
 y <- rep.int(0:Ns[2], Ns[1]+1)
@@ -50,46 +81,148 @@ if(tolower(method) %in% c("santner and snell","santner","snell")){
 }
 
 #Observed Statistic:
-TX[which(is.na(TX))]=0
-TXO<-TX[(Ns[2]+1)*data[1,1]+(data[1,2]+1),3]
+if(!(tolower(method) %in% c("csm","csm modified","csm approximate"))){
+   TX[which(is.na(TX))]=0
+   TXO<-TX[(Ns[2]+1)*data[1,1]+(data[1,2]+1),3]
+}
+
+if(tolower(method)=="csm approximate"){
+   twosidedLess=(data[1,1]/Ns[1] <= data[1,2]/Ns[2])
+   Tbls={}
+   if (tolower(alternative)=="two.sided"){
+      TX<-matrix(c(x,y,apply(matrix(c(x,Ns[1]-x,y,Ns[2]-y),(Ns[1]+1)*(Ns[2]+1),4),
+          1,FUN=function(tbls){maxPvalue(matrix(tbls,2,2))$pvalue})),(Ns[1]+1)*(Ns[2]+1),3)
+   } else {
+      otherTbls={}
+      for(a in 0:Ns[1]){
+      for(c in 0:Ns[2]){
+         if(tolower(alternative)=="less" | (alternative=="two.sided" & twosidedLess)){
+            if((a<data[1,1] & c>=data[1,2])|(a<=data[1,1] & c>data[1,2])){Tbls<-rbind(Tbls,c(a,c))
+            } else if ((a<data[1,1] & c<data[1,2])|(a>data[1,1] & c>data[1,2])){otherTbls<-rbind(otherTbls,c(a,c))}
+         } else {
+            if((a>data[1,1] & c<=data[1,2])|(a>=data[1,1] & c<data[1,2])){Tbls<-rbind(Tbls,c(a,c))
+            } else if ((a>data[1,1] & c>data[1,2])|(a<data[1,1] & c<data[1,2])){otherTbls<-rbind(otherTbls,c(a,c))}
+         }
+      }}
+      otherTbls<-rbind(otherTbls,data[1,])
+      TX<-matrix(c(otherTbls,apply(otherTbls,1,FUN=function(tbls){maxPvalue(matrix(tbls,1,2))$pvalue})),dim(otherTbls)[1],3)
+   }
+
+   if(!is.matrix(TX)){TX<-matrix(TX,ncol=3)}
+   TXO<-max(TX[apply(matrix(TX[,1:2],ncol=2), 1, function(x) all(x == data[1,])),3])
+   Tbls<-rbind(Tbls,TX[TX[,3] <= TXO,1:2])
+}
+
+#CSM test:
+if(tolower(method) %in% c("csm","csm modified")){
+
+   #When the test is two-sided, do a one-sided test then add the other more
+   #extreme tables at the end.  Have to choose which one-sided test to do though
+   twosidedLess=(data[1,1]/Ns[1] <= data[1,2]/Ns[2])
+
+   if(tolower(method)=="csm"){
+      Tbls={}
+      if(tolower(alternative)=="less" | (alternative=="two.sided" & twosidedLess)){Tbls<-matrix(c(0,Ns[2]),1,2)
+      } else { Tbls<-matrix(c(Ns[1],0),1,2)}
+   
+      #AC represents the pair(s) of (a,c) being checked
+      if(tolower(alternative)=="less" | (alternative=="two.sided" & twosidedLess)){
+         AC<-matrix(c(0,Tbls[1,2]-1,Tbls[1,1]+1,Ns[2]),2,2,byrow=T)
+         if(AC[2,1]>Ns[1]){AC=as.matrix(AC[-2,])}
+         if(AC[1,2]<0){AC=as.matrix(AC[-1,])}
+      } else {
+         AC<-matrix(c(Ns[1],Tbls[1,2]+1,Tbls[1,1]-1,0),2,2,byrow=T)
+         if(AC[2,1]<0){AC=as.matrix(AC[-2,])}
+         if(AC[1,2]>Ns[2]){AC=as.matrix(AC[-1,])}
+      }
+   } else {
+      Tbls={}
+      for(a in 0:Ns[1]){
+      for(c in 0:Ns[2]){
+         if(tolower(alternative)=="less" | (alternative=="two.sided" & twosidedLess)){
+            if((a<data[1,1] & c>=data[1,2])|(a<=data[1,1] & c>data[1,2])){Tbls<-rbind(Tbls,c(a,c))}
+         } else {
+            if((a>data[1,1] & c<=data[1,2])|(a>=data[1,1] & c<data[1,2])){Tbls<-rbind(Tbls,c(a,c))}
+         }
+      }}
+
+      #AC represents the pair(s) of (a,c) being checked
+      if(tolower(alternative)=="less" | (alternative=="two.sided" & twosidedLess)){
+         AC<-matrix(c(0,data[1,2]-1,data[1,],data[1,1]+1,Ns[2]),3,2,byrow=T)
+         if(AC[1,2]<0){AC=as.matrix(AC[-1,])}
+         if(AC[dim(AC)[1],1]>Ns[1]){AC=as.matrix(AC[-dim(AC)[1],])}
+      } else {
+         AC<-matrix(c(Ns[1],data[1,2]+1,data[1,],data[1,1]-1,0),3,2,byrow=T)
+         if(AC[1,2]>Ns[2]){AC=as.matrix(AC[-1,])}
+         if(AC[dim(AC)[1],1]<0){AC=as.matrix(AC[-dim(AC)[1],])}
+      }
+   }
+   TXO=NA
+
+   if(tolower(alternative)=="two.sided"){
+      Tbls<-rbind(Tbls,matrix(c(Ns[1]-Tbls[,1],Ns[2]-Tbls[,2]),dim(Tbls)[1],2))
+      if(any(duplicated(Tbls))){Tbls<-matrix(Tbls[-which(duplicated(Tbls)),],ncol=2)}
+   }
+
+   if(is.null(Tbls)){Tbls<-matrix(data[1,],ncol=2)}
+
+   while(sum(apply(Tbls, 1, function(x) all(x == data[1,])))==FALSE){
+
+      #Calculate the possible more extreme test statistic:
+      CcondAC<-rep(0,dim(AC)[1])
+      for(j in 1:dim(AC)[1]){
+         if(tolower(alternative)=='two.sided'){CcondAC[j]<-maxPvalue(rbind(Tbls,AC[j,],c(Ns[1]-AC[j,1],Ns[2]-AC[j,2])))$pvalue
+         } else {CcondAC[j]<-maxPvalue(rbind(Tbls,AC[j,]))$pvalue}
+      }
+
+      addRow<-which(round(CcondAC,digits=10)==min(round(CcondAC,digits=10)))
+      add<-matrix(AC[addRow,],length(addRow),2)
+      if(tolower(alternative)=="two.sided"){
+         Tbls<-rbind(Tbls,add,matrix(c(Ns[1]-add[,1],Ns[2]-add[,2]),length(addRow),2))
+      } else {
+         Tbls<-rbind(Tbls,add)
+      }
+
+      AC=matrix(AC[-addRow,],ncol=2)
+
+      if(alternative=="less" | (alternative=="two.sided" & twosidedLess)){
+         newAdd<-matrix(c(add[,1],add[,1]+1,add[,2]-1,add[,2]),ncol=2)
+      } else {
+         newAdd<-matrix(c(add[,1],add[,1]-1,add[,2]+1,add[,2]),ncol=2)
+      }
+      AC<-rbind(AC,newAdd[!is.na(newAdd[,1]),])
+      if(any(duplicated(AC))){AC<-matrix(AC[-which(duplicated(AC)),],ncol=2)}
+
+      if(alternative=="less" | (alternative=="two.sided" & twosidedLess)){
+         AC<-AC[order(AC[,1],-AC[,2]),]
+         AC<-AC[!duplicated(AC[,1]),]
+         AC<-AC[order(AC[,2],AC[,1]),]
+         AC<-AC[!duplicated(AC[,2]),]
+      } else {
+         AC<-AC[order(AC[,1],AC[,2]),]
+         AC<-AC[!duplicated(AC[,1]),]
+         AC<-AC[order(AC[,2],-AC[,1]),]
+         AC<-AC[!duplicated(AC[,2]),]
+      }
+      AC<-matrix(AC[AC[,1]>=0 & AC[,1]<=Ns[1] & AC[,2]>=0 & AC[,2]<=Ns[2],],ncol=2)
+
+   }
+
+}
 
 #Find tables that have a test statistic as or more extreme than the observed statistic:
-if(tolower(method) != "boschloo"){
-   if(tolower(alternative)=="greater"){Tbls<-TX[which(TX[,3]>=TXO),]
-   } else if(tolower(alternative)=="less"){Tbls<-TX[which(TX[,3]<=TXO),]
-   } else if(tolower(alternative)=="two.sided"){Tbls<-TX[which(abs(TX[,3])>=abs(TXO)),]}
-} else {Tbls<-TX[which(TX[,3]<=TXO),]}
-
+if(!(tolower(method) %in% c("boschloo","csm","csm modified","csm approximate"))){
+   if(tolower(alternative)=="greater"){Tbls<-matrix(TX[which(TX[,3]>=TXO),],ncol=3)
+   } else if(tolower(alternative)=="less"){Tbls<-matrix(TX[which(TX[,3]<=TXO),],ncol=3)
+   } else if(tolower(alternative)=="two.sided"){Tbls<-matrix(TX[which(abs(TX[,3])>=abs(TXO)),],ncol=3)}
+} else if (tolower(method) %in% c("boschloo")){Tbls<-matrix(TX[which(TX[,3]<=TXO),],ncol=3)}
 
 #Search for the maximum p-value:
-#Instead of calculating the binomial probabilities several times for a more extreme cell,
-#calculate the probability once and then combine with other more extreme cells:
-if(!is.matrix(Tbls)){Tbls<-t(Tbls)}
-xTbls<-Tbls[,1]
-yTbls<-Tbls[,2]
-nTbls<-length(xTbls) # Number of 'as or more extreme' tables
-
-x.unique<-unique(xTbls)
-y.unique<-unique(yTbls)
-lxu<-length(x.unique)
-lyu<-length(y.unique)
-
-xnr<-max(xTbls)+1
-A<-matrix(nrow=xnr, ncol=npNumbers)
-cellnr<-rep.int(x.unique+1, npNumbers)+xnr*rep(seq(npNumbers)-1, each=lxu)
-A[cellnr]<-dbinom(rep.int(x.unique, npNumbers), Ns[1], rep(int, each=lxu))
-
-ynr<-max(yTbls)+1
-B<-matrix(nrow=ynr, ncol=npNumbers)
-cellnr<-rep.int(y.unique+1, npNumbers)+ynr*rep(seq(npNumbers)-1, each=lyu)
-B[cellnr]<-dbinom(rep.int(y.unique, npNumbers), Ns[2], rep(int, each=lyu))
-
-prob<-A[xTbls+1,]*B[yTbls+1,]
-prob<-colSums(prob, dims=1)
-
-np<-int[which(prob==max(prob))]
-pvalue<-max(prob)+beta
-
+if(any(duplicated(Tbls))){stop("TABLES SHOULD NOT BE DUPLICATED: CHECK CODE")}
+maxP<-maxPvalue(Tbls)
+prob<-maxP$prob
+pvalue<-maxP$pvalue
+np<-maxP$np
 
 #Refine the p-value using the optimise function
 if(ref.pvalue==TRUE){
@@ -102,17 +235,20 @@ if(ref.pvalue==TRUE){
       refNp[i]<-ref$maximum
    }
    np<-refNp[refPvalue==max(refPvalue)]
-   pvalue<-refPvalue[refPvalue==max(refPvalue)]
+   pvalue<-max(refPvalue[refPvalue==max(refPvalue)])
 }
+
+#Note: if beta is large, can have a p-value greater than 1.  Cap at 1
+pvalue[pvalue>1]<-1
 
 #Plot p-value vs np
 if(to.plot==TRUE){
    plot(int,prob+beta,xlim=c(floor(min(int)*10)/10,ceiling(max(int)*10)/10),
    ylim=c(0,max(pvalue)),xlab="np",ylab="P-value",main="P-value as a function of the nuisance parameter")
-   points(np,pvalue,col="red",pch=21,bg="red")
+   points(np,rep(pvalue,length(np)),col="red",pch=21,bg="red")
 }
 
-list<-list(method=method,p.value=pvalue,test.statistic=TXO,np=np,np.range=c(min(int),max(int)))
+list<-list(method=method,p.value=max(pvalue),test.statistic=TXO,np=np,np.range=c(min(int),max(int)))
 return(list)
 
 }
